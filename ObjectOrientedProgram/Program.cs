c2776cecf81d47c8520c0e194baf2975b2acbc9f
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-09 01:34:15 -0800

    Solution Added

---
+++ ObjectOrientedProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using ObjectOrientedProgram.InventoryManagement;
+using System;
+
+namespace ObjectOrientedProgram
+{
+    public class Program
+    {
+        const string INVENTORY_DATA_FILE_PATH = @"C:\Users\hp\Desktop\CsharpCode\Json\ObjectOrientedProgram\InventoryManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Select Programs\n 1.Inventory Management ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Inventory inventory = new Inventory();
+                        inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                }
+            }
+        }
+    }
+}
