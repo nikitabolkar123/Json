d1ef1f5cce442580bfe12a0bb5d417c081afe9d7
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-09 01:50:33 -0800

    Solution Added

---
+++ ObjectOrientedProgram/InventoryManagementSystem/InventoryManagement.cs
@@ -0,0 +1,14 @@
+﻿using ObjectOrientedProgram.InventoryManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgram.InventoryManagementSystem
+{
+    public class InventoryManagement
+    {
+        public List<InventoryDetails>? RiceList { get; set; }
+        public List<InventoryDetails>? WheatList { get; set; }
+        public List<InventoryDetails>? PulsesList { get; set; }
+    }
+}
--- ObjectOrientedProgram/Program.cs
+++ ObjectOrientedProgram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ObjectOrientedProgram.InventoryManagement;
+using ObjectOrientedProgram.InventoryManagementSystem;
 using System;
 
 namespace ObjectOrientedProgram
@@ -6,17 +7,32 @@ namespace ObjectOrientedProgram
     public class Program
     {
         const string INVENTORY_DATA_FILE_PATH = @"C:\Users\hp\Desktop\CsharpCode\Json\ObjectOrientedProgram\InventoryManagement\Inventory.json";
+        const string INVENTORYDETAILS_DATA_FILE_PATH = @"C:\Users\hp\Desktop\CsharpCode\Json\ObjectOrientedProgram\InventoryManagementSystem\InventoryDetails.json";
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.WriteLine("Select Programs\n 1.Inventory Management ");
+                Console.WriteLine("Select Programs\n 1.Inventory Management\n2)Inventory Management System");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         Inventory inventory = new Inventory();
                         inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                    case 2:
+                        InventoryFactory inventoryFactory = new InventoryFactory();
+                        inventoryFactory.ReadJsonFile(INVENTORYDETAILS_DATA_FILE_PATH);
+                        InventoryDetails details = new InventoryDetails()
+
+                        {
+                            Name = "a",
+                            Weight = 5,
+                            Price = 25
+                        };
+                        inventoryFactory.AddInventory("WheatList", details);
+                        //inventoryFactory.DeleteInventory("WheatList", "a");
+                        inventoryFactory.WriteTojson(INVENTORYDETAILS_DATA_FILE_PATH);
                         break;
                 }
             }
